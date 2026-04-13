46da6ddbe3eb92d1014f9587875e844caa4c3e47
Laureano Gabriel Sanchez Aguilar <laureano.gabriel.aguiar@ibm.com>
2026-04-13 14:46:27 -0600

    Use relative paths to /Users/laureanosanchez

--- llama_cpp/gemma-4-26b-A4B-Q4_K_M-llama_cpp.sh
+++ llama_cpp/gemma-4-26b-A4B-Q4_K_M-llama_cpp.sh
@@ -2,14 +2,14 @@
 
 set +x
 
-/home/laureano-3090/Projects/llm_scripts/llama.cpp/build/bin/llama-server \
-  -m ~/Projects/models/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
-  --alias gemma-4-26b-Q4 \
-  --n-gpu-layers 999 \
-  --host 0.0.0.0 \
-  --port 11434 \
-  --temp 1.0 \
-  --top-p 0.95 \
-  --top-k 64 \
-  --chat-template-file ~/Projects/llm_scripts/llama.cpp/models/templates/google-gemma-4-31B-it.jinja \
-  --chat-template-kwargs '{"enable_thinking":true}'
+$HOME/Projects/llm_scripts/llama.cpp/build/bin/llama-server \
+	-m $HOME/Projects/models/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf \
+	--alias gemma-4-26b-Q4 \
+	--n-gpu-layers 999 \
+	--host 0.0.0.0 \
+	--port 11434 \
+	--temp 1.0 \
+	--top-p 0.95 \
+	--top-k 64 \
+	--chat-template-file $HOME/Projects/llm_scripts/llama.cpp/models/templates/google-gemma-4-31B-it.jinja \
+	--chat-template-kwargs '{"enable_thinking":true}'
--- llama_cpp/qwen3.5-27b-claude-Q4_K_M-llama_cpp.sh
+++ llama_cpp/qwen3.5-27b-claude-Q4_K_M-llama_cpp.sh
@@ -2,10 +2,9 @@
 
 set +x
 
-/home/laureano-3090/Projects/llm_scripts/llama.cpp/build/bin/llama-server \
-  -m ~/Projects/models/qwen3.5-27b-claude-Q4_K_M.gguf \
-  --alias qwen3.5-27b-claude-Q4 \
-  --n-gpu-layers 999 \
-  --host 0.0.0.0 \
-  --port 11434 \
-
+$HOME/Projects/llm_scripts/llama.cpp/build/bin/llama-server \
+	-m $HOME/Projects/models/qwen3.5-27b-claude-Q4_K_M.gguf \
+	--alias qwen3.5-27b-claude-Q4 \
+	--n-gpu-layers 999 \
+	--host 0.0.0.0 \
+	--port 11434
